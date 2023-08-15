d43893d119f87db6d7217e604749360d6d669d16
Parag <paragdurafe25@gmail.com>
2023-08-16 00:11:35 +0530

    :star: added models and templates

--- thesolequest/shoes/admin.py
+++ thesolequest/shoes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Customer, ShoeCondition, Product, Order, OrderItem, Shipping
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(ShoeCondition)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Shipping)
