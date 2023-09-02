0f15360af05465a6fce9ce5a77c78964ae4a5f0e
Parag <paragdurafe25@gmail.com>
2023-09-02 21:38:01 +0530

    :dizzy: Tailwind updated

--- thesolequest/shoes/models.py
+++ thesolequest/shoes/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
