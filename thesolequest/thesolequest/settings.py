593e12274d2d39e11e543db60f9c647d0ba0780e
ParagD25 <parag@Parags-MacBook-Air.local>
2023-10-12 23:35:06 +0530

    :bell: Stripe Data Added

--- thesolequest/thesolequest/settings.py
+++ thesolequest/thesolequest/settings.py
@@ -129,3 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/shoes")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STRIPE_PUBLIC_KEY = ""
+STRIPE_SECRET_KEY = ""
+STRIPE_WEBHOOK_SECRET = ""
